1048ab6baa5441968b72f0738ed1e2944bb1b14f
Harsha Vardhan M <harsha.menda21@gmail.com>
2025-08-19 15:22:05 +0530

    Add files via upload

---
+++ conditionalState.js
@@ -0,0 +1,16 @@
+// let day=3;
+// switch(day){
+//     case 1:console.log("Monday");break;
+//     case 2:console.log("Tuesday");break;
+//     case 3:console.log("Wednesday");break;
+//     default:console.log("try Some Other Day");
+// }
+
+let score=75;
+if(score>=90){
+    console.log("Grade A");
+}else if(score>=75){
+    console.log("Grade B");
+}else{
+    console.log("Grade C");
+}
